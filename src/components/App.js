9c80c546263e0ba8f4645ac1c8e0f40d43c0fe61
Kalyani Namdev Kharat <kalyanikharat84@gmail.com>
2025-12-05 12:07:24 +0530

    Update the code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import RelativeList from './Relativelist';   // import your component
 
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>Diwali Shopping Visits</h1>
+      <p>Here are the relatives I plan to visit:</p>
+      <RelativeList />   {/* render the list here */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
